30521c8724d877463d2e8478bf58c30887f274b4
Travis Cooper <travis@evolvingcoder.com>
2014-02-25 00:02:13 -0500

    untested basic setup

--- budgetbasics/baseapp.py
+++ budgetbasics/baseapp.py
@@ -2,13 +2,13 @@ import web
 
 
 db = web.database(dbn='mysql', user='test', pw='test123', db='budgetbasics')
+globals = {'hasattr': hasattr}
 
-render = web.template.render('templates/')
-
-
+render = web.template.render('templates/', base='base', globals=globals)
 
 urls = (
     '/', 'index',
+    '/expense_form', 'expense_form'
     '/add_expense', 'add_expense'
 )
 
@@ -17,13 +17,23 @@ app = web.application(urls, globals())
 
 class index:
     def GET(self):
-        return render.index()
+        var_where = "expenses.type = 'Variable'"
+        variable_exp = list(db.select('expenses', where=var_where))
+        fixed_where = "expenses.type = 'Fixed'"
+        fixed_exp = list(db.select('exenses', where=fixed_where))
+         
+        return render.index(variable_exp, fixed_exp)
+
+class expense_form:
+    def GET(self):
+        return render.expense_form()
 
 class add_expense:
-    def GET(self):
-        pass
     def POST(self):
-        pass
+        form = web.input()
+        expense = db.insert('expenses', name=form.exp_name,
+            type=form.exp_type, amount=form.exp_amount)
+        raise web.seeother('/')
 
 if __name__ == "__main__":
     app.run()

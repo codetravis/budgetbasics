f49d46d425afb872de5b337f5c3fd1b352eea3ef
Travis Cooper <travis@evolvingcoder.com>
2014-02-25 19:07:02 -0500

    added date to expense form and to expense table

--- budgetbasics/baseapp.py
+++ budgetbasics/baseapp.py
@@ -8,11 +8,11 @@ render = web.template.render('templates/', base='base', globals=globals)
 
 urls = (
     '/', 'index',
-    '/expense_form', 'expense_form'
+    '/expense_form', 'expense_form',
     '/add_expense', 'add_expense'
 )
 
-app = web.application(urls, globals())
+app = web.application(urls, locals())
 
 
 class index:
@@ -20,7 +20,7 @@ class index:
         var_where = "expenses.type = 'Variable'"
         variable_exp = list(db.select('expenses', where=var_where))
         fixed_where = "expenses.type = 'Fixed'"
-        fixed_exp = list(db.select('exenses', where=fixed_where))
+        fixed_exp = list(db.select('expenses', where=fixed_where))
          
         return render.index(variable_exp, fixed_exp)
 
@@ -31,8 +31,9 @@ class expense_form:
 class add_expense:
     def POST(self):
         form = web.input()
-        expense = db.insert('expenses', name=form.exp_name,
-            type=form.exp_type, amount=form.exp_amount)
+        expense = db.insert('expenses', name=form.exp_name, 
+            expense_date=form.exp_date, type=form.exp_type, 
+            amount=form.exp_amount)
         raise web.seeother('/')
 
 if __name__ == "__main__":
